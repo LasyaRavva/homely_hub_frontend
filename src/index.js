6fc2d56ef79c593f69910196f814a410bf117669
lasyaravva <LasyaRavva>
2026-03-03 21:07:18 +0530

    Fix Netlify API base URL and invalid integrity attribute

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 import './index.css';
 import App from './App';
 import store from "./Store/store";
 import {Provider} from "react-redux";
+
+axios.defaults.baseURL =
+  process.env.REACT_APP_API_URL || "http://127.0.0.1:8000";
+axios.defaults.withCredentials = true;
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
